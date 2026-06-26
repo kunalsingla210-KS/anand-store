aadd111522d8a384d2a13fddc90a365777ba3e94
kunalsingla210-KS <rishisingla1983@gmail.com>
2026-06-26 22:11:24 +0530

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Anand Store</h1>
+        <p>Your shopping destination</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
